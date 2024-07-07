9bfcc1cb5ca0495f4f5d0a3a7c9bcf3d3280f838
Shabbir8788 <hasanalshabbir257@gmail.com>
2024-07-07 23:54:59 +0600

    Enrolled Course

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -10,6 +10,7 @@ import { CourseRoutes } from '../modules/courses/course.route'
 import { SemesterRegistrationRoutes } from '../modules/semesterRegistration/semesterRegistration.route'
 import { offeredCourseRoutes } from '../modules/offeredCourse/offeredCourse.route'
 import { AuthRoutes } from '../modules/auth/auth.route'
+import { EnrolledCourseRoutes } from '../modules/EnrolledCourse/enrolledCourse.route'
 
 const router = Router()
 
@@ -67,6 +68,11 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
+  },
+
+  {
+    path: '/enrolled-courses',
+    route: EnrolledCourseRoutes,
   },
 ]
 
